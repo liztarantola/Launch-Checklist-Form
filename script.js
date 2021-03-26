b37fded945487df28aa45317c5c46143acb2b701
Elizabeth Tarantola <elizabethtarantola@Elizabeths-MacBook-Pro.local>
2021-03-25 21:50:31 -0500

    final commit

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
-let form = document.querySelector("form");
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      return response.json();
+   }).then(function(json) {
+      console.log(json);
+      const destination = document.querySelector("#missionTarget");
+      let planet = 
+         `
+         <h2 id="destination">Mission Destination</h2>
+         <ol>
+             <li>Name: ${json[0].name} </li>
+             <li>Diameter:${json[0].diameter}</li>
+             <li>Star: ${json[0].star}</li>
+             <li>Distance from Earth: ${json[0].distance}</li>
+             <li>Number of Moons: ${json[0].moons}</li>
+         </ol>
+         <img src="${"https://www.nasa.gov/sites/default/files/images/587837main_Kepler16_transit_art2_full.jpg"}"></img>
+         `
+         destination.innerHTML = planet;
+   });
+      
+      const form = document.querySelector("form");
       form.addEventListener("submit", function(event) {
          event.preventDefault();
          let pilotInput = document.querySelector("input[name=pilotName]");
@@ -12,14 +32,39 @@ let form = document.querySelector("form");
          let fuel = Number(fuelInput.value);
          let cargoInput= document.querySelector("input[name=cargoMass]");
          let cargo = Number(cargoInput.value);
-         
+         let status = document.getElementById('launchStatus');
+         let pilotInfo = document.getElementById('pilotStatus');
+         let copilotInfo = document.getElementById("copilotStatus");
+         let fuelInfo = document.getElementById('fuelStatus');
+         let cargoInfo = document.getElementById('cargoStatus');
+
          if (pilot === "" || copilot === "" || fuel ===  "" || cargo === "") {
             alert("All fields are required!");
             
-         } else if (isNaN(pilot) === false || isNaN(copilot) === false || isNaN(fuel) || isNaN(cargo)) {
-            alert("Incorrect Data");
+            } else if (isNaN(pilot) === false || isNaN(copilot) === false || isNaN(fuel)|| isNaN(cargo)) {
+            alert("Incorrect Data!");
+
+            } else if (fuel < 10000) {
+               document.getElementById("faultyItems").style.visibility = "visible";
+               status.innerHTML = "Shuttle not ready for launch.";
+               status.style = "color:red";
+               pilotInfo.innerHTML = `Pilot ${pilot} is ready for launch.`;
+               copilotInfo.innerHTML = `Co-pilot ${pilot} is ready for launch.`; 
+               fuelInfo.innerHTML = "Fuel level too low for launch.";
             
-         }
+            } else if (cargo > 10000) {
+               document.getElementById("faultyItems").style.visibility = "visible";
+               status.innerHTML = "Shuttle not ready for launch.";
+               status.style = "color:red";
+               pilotInfo.innerHTML = `Pilot ${pilot} is ready for launch.`;
+               copilotInfo.innerHTML = `Co-pilot ${pilot} is ready for launch.`; 
+               cargoInfo.innerHTML = "Cargo mass too high for launch.";
+            
+               } else {
+                  document.getElementById("faultyItems").style.visibility = "visible";
+                  status.innerHTML = "Shuttle is ready for launch.";
+                  status.style = "color:green";
+               } 
       });
    });     
 
